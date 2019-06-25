d1e9276ffb621a770e5ad9a0b5876af1e266d3e1
Yash <yashgupta417@gmail.com>
2019-06-25 14:25:06 +0530

    post serializer is perfect now

--- mychat/api/serializers.py
+++ mychat/api/serializers.py
@@ -6,7 +6,7 @@ class PostSerializer(serializers.ModelSerializer):
     class Meta:
         model=Post
         fields='__all__'
-        read_only_fields=['id','likes','image']
+        read_only_fields=['id']
 # class UserInfoDetailSerializer(serializers.ModelSerializer):
 #     class Meta:
 #         model=UserInfo
