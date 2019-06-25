6e10eb1a3377bc8a28a25cf7d462d08cb949fb39
Yash <yashgupta417@gmail.com>
2019-06-25 13:43:29 +0530

    adding likes to read_only in  post serializer

--- mychat/api/serializers.py
+++ mychat/api/serializers.py
@@ -6,7 +6,7 @@ class PostSerializer(serializers.ModelSerializer):
     class Meta:
         model=Post
         fields=['id','image','title','message','posted_by','likes']
-        read_only_fields=['id',]
+        read_only_fields=['id','likes']
 # class UserInfoDetailSerializer(serializers.ModelSerializer):
 #     class Meta:
 #         model=UserInfo
